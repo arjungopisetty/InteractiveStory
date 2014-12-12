9b21ef381a7e928e7e6cf02a958d261c0d6024b4
Arjun Gopisetty <arjun.gopisetty@gmail.com>
2014-12-12 02:36:45 -0600

    Initial commit

---
+++ app/src/main/java/com/arjungopisetty/interactivestory/model/Choice.java
@@ -0,0 +1,31 @@
+package com.arjungopisetty.interactivestory.model;
+
+/**
+ * Created by ArjunGopisetty on 12/5/14.
+ */
+public class Choice {
+
+    private String mText;
+    private int mNextPage;
+
+    public Choice(String text, int nextPage) {
+        mText = text;
+        mNextPage = nextPage;
+    }
+
+    public String getText() {
+        return mText;
+    }
+
+    public void setText(String text) {
+        mText = text;
+    }
+
+    public int getNextPage() {
+        return mNextPage;
+    }
+
+    public void setNextPage(int nextPage) {
+        mNextPage = nextPage;
+    }
+}
